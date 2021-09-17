3e15d3c6c36c2e5a6de99e21b0b91ffa069e5de5
Konrad Haight <konradhaight@gmail.com>
2021-09-16 20:46:30 -0400

    Initial Commit

---
+++ ProjTwo/ClassTwo.cs
@@ -0,0 +1,7 @@
+namespace ProjTwo
+{
+    public class ClassTwo
+    {
+
+    }
+}
---
+++ ProjTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProjTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 1; i<=100; i++)
+            {
+                if(i%15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
